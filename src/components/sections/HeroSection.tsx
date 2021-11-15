ec7f46f0a484237323c7b47d42f152e1a35a52f1
João Alpalhão <5016958-joaoalpalhao@users.noreply.gitlab.com>
2021-11-15 19:05:41 +0000

    Edit HeroSection

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -3,35 +3,58 @@ import TypedWrapper from '../typed_wrapper';
 import stylex from "@ladifire-opensource/stylex";
 
 const styles = stylex.create({
-  main: {
-    position: 'relative',
-    height: '100vh',
+  section: {
     width: '100%',
+    height: '100vh',
+    position: 'relative',
     backgroundColor: 'var(--dark-background-color-black)',
-    //boxShadow: '0 -0.75em 2em 1em var(--dark-shadow-light)',
-    //zIndex: 2,
   },
-  text: {
+  main: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  content: {
+    width: '100%',
+    marginLeft: '4em',
+    marginRight: '4em',
+  },
+  typedOuter: {
+    width: '36em',
+    maxWidth: '36em',
+    height: '2.5em',
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
+  typed: {
     color: 'var(--dark-text-color-white)',
-    lineHeight: '1.5',
-    fontSize: '3.9975843648em',
+    fontSize: '2em',
   },
 })
 
 const options = {
-  strings: ['Passionate about coding.', 'Passionate about photography.', 'Passionate about comedy.'],
+  strings: ['Passionate about coding.', 'Passionate about photography.', 'Passionate about gaming.'],
   typeSpeed: 40,
   backSpeed: 40,
   startDelay: 1000,
   backDelay: 2000,
   loop: true,
-  showCursor: false,
+  cursorChar: '&nbsp',
+  autoInsertCss: false,
 }
 
 const HeroSection: React.FC = () => {
   return (
-    <div className={stylex(styles.main)}>
-      <TypedWrapper xstyle={styles.text} options={options}/>
+    <div className={stylex(styles.section)}>
+      <div className={stylex(styles.main)}>
+        <div className={stylex(styles.content)}>
+          <div className={stylex(styles.typedOuter)}>
+            <TypedWrapper xstyle={styles.typed} options={options}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
